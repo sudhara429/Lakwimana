62bbe1eb0c3b24ef4e4d06f8964bfa855f80f926
sudhara429 <sudhara.dananjaya@gmail.com>
2016-05-06 09:23:12 +0530

    Initial commit

---
+++ app/src/main/java/com/lakwimana/lakwimana/MainActivity.java
@@ -0,0 +1,189 @@
+package com.lakwimana.lakwimana;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //for webview loading
+        String url = "http://lakwimana.com/app.php" ; // Defining URL
+        WebView view = (WebView) this .findViewById (R.id.webView);   // synchronization object based on the id
+        WebSettings webSettings = view.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        view.loadUrl (url);    // URL that is currently open applications terload
+    }
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("lakwimana.com")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        WebView view = (WebView) this .findViewById (R.id.webView);
+        final WebView view2=view;
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_cart) {
+            // Handle the cart action
+            view.loadUrl("http://lakwimana.com/shopping_cart.php");
+        } else if (id == R.id.nav_new) {
+
+        } else if (id == R.id.nav_special) {
+
+        } else if (id == R.id.nav_checkout) {
+
+        } else if (id == R.id.nav_lang) {
+
+            CharSequence colors[] = new CharSequence[] {"English", "Sinhala"};
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Pick a Language");
+            builder.setItems(colors, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    switch (which) {
+                        case 1:
+                            // int which = -2
+                            view2.loadUrl("http://www.lakwimana.com/index.php?language=si");
+                            dialog.dismiss();
+                            break;
+                        case 0:
+                            view2.loadUrl("http://www.lakwimana.com/index.php?language=en");
+                            // int which = -3
+                            dialog.dismiss();
+                            break;
+                    }
+                    // the user clicked on colors[which]
+                }
+            });
+            builder.show();
+        } else if (id == R.id.nav_currency) {
+
+            CharSequence colors[] = new CharSequence[] {"USD", "LKR"};
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Pick a Currency");
+            builder.setItems(colors, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    switch (which) {
+                        case 1:
+                            // int which = -2
+                            view2.loadUrl("http://www.lakwimana.com/index.php?currency=USD");
+                            dialog.dismiss();
+                            break;
+                        case 0:
+                            view2.loadUrl("http://www.lakwimana.com/index.php?currency=LKR");
+                            // int which = -3
+                            dialog.dismiss();
+                            break;
+                    }
+                    // the user clicked on colors[which]
+                }
+            });
+            builder.show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
